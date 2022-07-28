48de17510a0d8117e031e6e9d04a6828285d1362
Mostafa Allam <mostafaallam602@gmail.com>
2022-07-28 18:47:18 +0200

    adding taggeditem manager class

--- tags/models.py
+++ tags/models.py
@@ -4,7 +4,20 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
+
+class TaggedItemManager(models.Manager):
+    def get_for_product(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(object_type = obj_type)
+        return TaggedItem.objects \
+        .select_related('tag') \
+        .filter(
+            content_type = content_type,
+            object_id = obj_id,
+        )
+
 class TaggedItem(models.Model):
+    # adding our manager object
+    objects = TaggedItemManager()
     #what tag applied to what object
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     # To define a generic relationship:
